a45fa3b1233297082682943d1422b35f2d8e160a
dung11205 <vandunght00000@gamil.com>
2024-11-16 22:23:13 +0700

    dung

--- index.js
+++ index.js
@@ -6,18 +6,18 @@ require('dotenv').config();
 const app = express();
 
 // Kết nối MongoDB
-mongoose.connect('mongodb+srv://dung:dung1234@dung.dhy4z.mongodb.net/?retryWrites=true&w=majority&appName=dung', {
-
-
-}).then(() => {
+mongoose
+  .connect('mongodb+srv://dung:dung1234@dung.dhy4z.mongodb.net/<your-database>?retryWrites=true&w=majority')
+  .then(() => {
     console.log('Connected to MongoDB Atlas');
-}).catch((err) => {
+  })
+  .catch((err) => {
     console.error('Error connecting to MongoDB:', err);
-});
+  });
 
 // Middleware
 app.use(cors());
-app.use(express.json()); // Thêm middleware để parse JSON requests
+app.use(express.json());
 
 // Định nghĩa route cho các API
 const tripRoute = require('./routes/trip.route');
@@ -29,10 +29,10 @@ app.use('/address', addressRoute);
 app.use('/trip', tripRoute);
 
 // Route chính
-app.get('/', (req, res) => res.send('Node.js'));
+app.get('/', (req, res) => res.send('Node.js API is running'));
 
 // Khởi động server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
