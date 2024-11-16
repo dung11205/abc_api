5e95559da7a7496ef265dfdb5eb75c6c61539723
dung11205 <vandunght00000@gamil.com>
2024-11-16 23:59:52 +0700

    dungd

--- index.js
+++ index.js
@@ -30,7 +30,8 @@ app.use('/trip', tripRoute);
 app.get('/', (req, res) => res.send('dmmdmdm'));
 
 // Khởi động server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
+
